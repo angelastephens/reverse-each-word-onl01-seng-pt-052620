5a23f7e6ab839568a98062322a8d8877e75fc678
Angela Stephens <angelastephens317@gmail.com>
2020-06-04 03:33:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@
   end 
   return_array.join(" ") 
   end
+  
+  def reverse_each_word(string)
+   array1= string.split (" ")
+   return_array = []
+   array1.collect do |string|
+     return_array << string.reverse
+  end 
+  return_array.join(" ") 
+  end
